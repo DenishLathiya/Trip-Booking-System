1ece52b39221641c385700f95c8bf8d2d330fa67
Denish Lathiya <denishlathiya999@gmail.com>
2025-07-19 20:25:43 +0530

    New Changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -56,10 +56,10 @@ const Footer = () => {
           <div>
             <h3 className="text-lg font-semibold mb-4">Contact Us</h3>
             <ul className="space-y-2 text-sm">
-              <li>/surat, Country= india </li>
-              <li>Phone: +91 7567236791</li>
-              <li>Email: yoyo@tourbuddy.com</li>
-            </ul> 
+              <li>Bela Gujarat</li>
+              <li>Phone: +91 234563456</li>
+              <li>Email: my@tourbuddy.com</li>
+            </ul>
           </div>
           <div>
             <h3 className="text-lg font-semibold mb-4">Follow Us</h3>
