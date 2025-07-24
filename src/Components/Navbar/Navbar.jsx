c1a3198eebd0f421c450532a4579538f87ab0a71
Denish Lathiya <denishlathiya999@gmail.com>
2025-07-24 18:56:56 +0530

    hovering and focus gap

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -102,7 +102,7 @@ const Navbar = () => {
 
   return (
     <header className="sticky mx-auto top-0 transition-all py-6 bg-transparent z-10">
-      <div className="bg-[#121f4d]/85 px-4 fixed w-full z-50 top-0 py-2">
+      <div className="bg-[#121f4d]/95 px-4 fixed w-full z-50 top-0 py-2">
         <div className="max-w-7xl mx-auto py-2 px-5 flex bg-transparent justify-between items-center">
           <Link to="/">
             <h1 className="text-2xl text-white font-bold">
